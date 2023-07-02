1277c43ba6b589443c8c74ecccc21ec589b51fa9
Marcio Curvello <marciocurvello@gmail.com>
2023-07-02 12:56:13 -0300

    update to rest

--- MoviesApi/Controllers/MovieController.cs
+++ MoviesApi/Controllers/MovieController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using MoviesApi.Models;
 
@@ -12,22 +13,27 @@ namespace MoviesApi.Controllers
 		private static int id = 0;
 
 		[HttpPost]
-		public void AddMovie([FromBody] Movie movie)
+		public IActionResult AddMovie([FromBody] Movie movie)
 		{
 			movie.Id = id++;
 			movies.Add(movie);
+			return CreatedAtAction(nameof(GetMovieById), new {id = movie.Id}, movie);
         }
 
 		[HttpGet]
-		public IEnumerable<Movie> GetAllMovies([FromQuery] int skip = 0, [FromQuery] int take = 50)
+		public IActionResult GetAllMovies([FromQuery] int skip = 0, [FromQuery] int take = 50)
 		{
-			return movies.Skip(skip).Take(take);
+			if (movies.Count == 0) return NoContent();
+
+            return Ok(movies.Skip(skip).Take(take));
 		}
 
         [HttpGet("{id}")]
-        public Movie? GetMovieById(int id)
+        public IActionResult GetMovieById(int id)
         {
-			return movies.FirstOrDefault(movie => movie.Id == id);
+			var movie = movies.FirstOrDefault(movie => movie.Id == id);
+			if (movie == null) return NotFound();
+			return Ok(movie);
         }
     }
 }
